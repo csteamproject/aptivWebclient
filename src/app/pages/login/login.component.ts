e26b17558c679c85a7d33f9c01523bb85e39ee06
xsalmon <xsalmon@nmsu.edu>
2018-11-09 12:37:05 -0700

    adds login erroring

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,6 +12,13 @@ import {
 import {
   HttpClient
 } from '@angular/common/http';
+import {
+  ItemsService
+} from '../../services/items/items.service';
+import {
+  Item
+} from '../../classes/item/item';
+import { ReturnStatement } from '@angular/compiler';
 
 
 @Component({
@@ -21,19 +28,19 @@ import {
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
-
+  LoginError: Number= 1;  
   constructor(private router: Router,
     public auth: AuthService) {}
 
   ngOnInit() {}
-
   TryToLogin(credentials) {
     this.auth.login(credentials).subscribe(data => {
       // console.log('LoginComponent: ', data);
       // if (true) { // TODO: Add a Checkbox to see if they want to be remembered for a period of time.
       //   localStorage.setItem('CurrentUser', JSON.stringify(data));
       // }
-      this.router.navigate(['inventory']);
+        this.router.navigate(['inventory']);
     });
+    this.LoginError=0;
   }
 }
