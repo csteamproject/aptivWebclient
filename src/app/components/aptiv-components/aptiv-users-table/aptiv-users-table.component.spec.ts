b160bfd2b6bbe7f1b7a4168f2c7252958e130be0
xsalmon <xsalmon@nmsu.edu>
2018-11-10 23:20:44 -0700

    adds login erroring

---
+++ src/app/components/aptiv-components/aptiv-users-table/aptiv-users-table.component.spec.ts
@@ -0,0 +1,60 @@
+import {
+    async,
+    ComponentFixture,
+    TestBed
+  } from '@angular/core/testing';
+  import {
+    UsersTableComponent
+  } from './users-table.component';
+  import {
+    FormsModule
+  } from '@angular/forms';
+  import {
+    RouterTestingModule
+  } from '@angular/router/testing';
+  import {
+    HttpClientModule
+  } from '@angular/common/http';
+  import {
+    AddItemModalComponent
+  } from '../add-item-modal/add-item-modal.component';
+  import {
+    DeleteModalComponent
+  } from '../delete-modal/delete-modal.component';
+  import {
+    EditItemModalComponent
+  } from '../edit-item-modal/edit-item-modal.component';
+  
+  describe('AptivTableComponent', () => {
+    let component: UsersTableComponent;
+    let fixture: ComponentFixture < UsersTableComponent > ;
+  
+    beforeEach(async (() => {
+      TestBed.configureTestingModule({
+          imports: [
+            FormsModule,
+            RouterTestingModule,
+            HttpClientModule
+          ],
+          declarations: [UsersTableComponent,
+            AddItemModalComponent,
+            DeleteModalComponent,
+            EditItemModalComponent
+          ]
+        })
+        .compileComponents();
+    }));
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(UsersTableComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  
+    // TODO: Write a testcase to check and make sure all data is being passed in properly to the aptiv-table component
+  });
+  
