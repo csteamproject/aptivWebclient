f251ef2ce341a3b91e7c5eae2387f9fe7de8feaf
gilbertcar <gcarr20@nmsu.edu>
2018-11-15 14:50:24 -0700

    csv file

--- src/app/pages/locations/locations.component.ts
+++ src/app/pages/locations/locations.component.ts
@@ -15,13 +15,28 @@ import {
   styleUrls: ['./locations.component.css']
 })
 export class LocationsComponent implements OnInit {
-
   Locations: Location[] = [];
+  fileToUpload: any;
   constructor(private locationsService: LocationsService) {}
 
   ngOnInit() {
     this.locationsService.getLocations().subscribe((data: Location[]) => {
       this.Locations = data;
     });
+  }
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+    if (this.fileToUpload !== null) {
+    console.log('the file ' + files.item(0));
+    console.log('size ' + this.fileToUpload.name);
+    const reader = new FileReader();
+    reader.onload = () => {
+    console.log(reader.result);
+  };
+  console.log('reading');
+  console.log('read: ' + reader.readAsText(this.fileToUpload));
+  // console.log(reader.readAsText(this.fileToUpload));
+}
   }
 }
